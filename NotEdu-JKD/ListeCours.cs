31fdeb38c5d8f751636716e4ffc0ce046bdd4f29
Jeremy Gabillet <jeremy.gabillet@gmail.com>
2021-09-30 10:54:55 +0200

    Ajout de la classe ListeCours
    Transfert de la methode de suppression d'un cours vers la liste de cours

---
+++ NotEdu-JKD/ListeCours.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotEdu_JKD
+{
+    class ListeCours
+    {
+        private List<Cours> _listeDesCours = new List<Cours>();
+        public List<Cours> ListeDesCours { get { return _listeDesCours; } }
+        public ListeCours()
+        {
+        }
+        public void AjouterCours(Cours nouveauCours)
+        {
+            _listeDesCours.Add(nouveauCours);
+        }
+
+        /*Suppression de toutes les occurences d'un cours.*/
+        public void SuppressionCours(Cours cours)
+        {
+            Console.Write("/!\\ La suppression d'un cours entraîne la suppression de toutes les notes " +
+                "et appréciations qui lui sont liées. \n" +
+                $"Voulez-vous vraiment supprimer le cours {cours.Titre}? (Oui/Non) ");
+            string reponseSuppression = Console.ReadLine().ToLower();
+
+            if (reponseSuppression == "oui")
+            {
+                int id = cours.Id;
+                /* Loop à travers tout les élèves, dans toutes leurs notes pour trouver l'ID correspondant*/
+            }
+            else
+            {
+                Console.WriteLine("Annulation de la suppression du cours.");
+                return;
+            }
+        }
+    }
+}
