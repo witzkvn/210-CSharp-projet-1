c018d7b729e1d9b026e182275c2f4d93f52c08ef
Jeremy Gabillet <jeremy.gabillet@gmail.com>
2021-10-07 14:48:03 +0200

    Test des nouvelles fonctions

--- NotEdu-JKD/Program.cs
+++ NotEdu-JKD/Program.cs
@@ -10,7 +10,17 @@ namespace NotEdu_JKD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\tTest de tabulations");
+            ListeEleves campus = new ListeEleves();
+            ListeCours programme = new ListeCours();
+            Eleve kevin = new Eleve("Witz", "Kévin", new DateTime(1995, 08, 01));
+            Eleve naim = new Eleve("El Moudni", "Naïm", new DateTime(1992, 10, 17));
+            campus.AjouterEleveDansListe(kevin);
+            campus.AjouterEleveDansListe(naim);
+            programme.AjouterCours(new Cours("Maths"));
+            kevin.ListeNotes.Add(new Note(programme.ListeDesCours[0], 13, "Bien" ));
+            naim.ListeNotes.Add(new Note(programme.ListeDesCours[0], 17, "Très bien"));
+            programme.SuppressionCours(campus);
+
             Console.ReadLine();
         }
     }
