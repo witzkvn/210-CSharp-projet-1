47f166da0ad3d21281920ad01c09216d389cf8cd
witzkvn <witz.kevin05@gmail.com>
2021-10-08 12:31:12 +0200

    deplacement de la fonction creation d'un eleve dans ListeEleves, pour avoir ajout et suppresion dans la même classe

--- NotEdu-JKD/ListeEleves.cs
+++ NotEdu-JKD/ListeEleves.cs
@@ -8,12 +8,7 @@ namespace NotEdu_JKD
 {
     static class ListeEleves
     {
-        static private Dictionary<int, Eleve> _listeDesEleves = new Dictionary<int, Eleve>()
-        {
-            {1, new Eleve("NOM1","Prenom1", DateTime.Parse("11/11/2001") ) },
-            {2, new Eleve("NOM2","Prenom2", DateTime.Parse("07/07/2007") ) },
-            {3, new Eleve("NOM3","Prenom3", DateTime.Parse("13/03/2003") ) },
-        };
+        static private Dictionary<int, Eleve> _listeDesEleves = new Dictionary<int, Eleve>();
         // TODO récupérer liste des élèves du JSON et la mettre dans _listeEleves
         private static int _idGlobalEleve = _listeDesEleves.Count == 0 ? 0 : _listeDesEleves.Keys.Max();
 
@@ -25,6 +20,86 @@ namespace NotEdu_JKD
             Console.WriteLine($"Ajout de l'élève {nouvelEleve.Nom} {nouvelEleve.Prenom} réussi.");
             ActualiserListeJSON();
             // ajouter appel méthode retour au menu
+        }
+
+        static public void CreerNouvelEleve()
+        {
+            string nom = "";
+            string prenom = "";
+            string dateNaissance = "";
+
+            while (!Utilitaire.VerifUniquementLettres(nom) || nom == "")
+            {
+                Console.Write("Quel est le nom du nouvel élève ? (lettres uniquement -- 'retour' pour quitter) : ");
+                nom = Console.ReadLine().ToLower();
+                if (nom == "retour") break;
+            }
+            if (nom == "retour")
+            {
+                // appeler méthode de menu pour revenir au menu principal
+                Console.WriteLine("Retour menu principal");
+            }
+
+            while (!Utilitaire.VerifUniquementLettres(prenom) || prenom == "")
+            {
+                Console.Write("Quel est le prénom du nouvel élève ? (lettres uniquement -- 'retour' pour quitter) : ");
+                prenom = Console.ReadLine().ToLower();
+                if (prenom == "retour") break;
+            }
+            if (prenom == "retour")
+            {
+                // appeler méthode de menu pour revenir au menu principal
+                Console.WriteLine("Retour menu principal");
+            }
+
+            while (!Utilitaire.VerifFormatDate(dateNaissance) || dateNaissance == "")
+            {
+                Console.Write("Quel est la date de naissance du nouvel élève ? (format JJ/MM/AAAA -- 'retour' pour quitter) : ");
+                dateNaissance = Console.ReadLine().ToLower();
+                if (dateNaissance == "retour") break;
+            }
+            if (dateNaissance == "retour")
+            {
+                // appeler méthode de menu pour revenir au menu principal
+                Console.WriteLine("Retour menu principal");
+            }
+
+            DateTime dateNaissanceValideEleve = Utilitaire.StringToDate(dateNaissance);
+            Eleve nouvelEleve = new Eleve(nom.ToUpper(), Utilitaire.PremiereLettreMajuscule(prenom), dateNaissanceValideEleve);
+
+            string choixAction;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Voici le récapitulatif des informations saisies... ");
+                Console.WriteLine();
+                nouvelEleve.AfficherInfoEleve();
+                Console.WriteLine();
+                Console.WriteLine("Que souhaitez-vous faire ? (choisir l'action par son numéro)");
+                Console.WriteLine();
+                Console.WriteLine("1 - Valider la saisie et ajouter l'élève au campus");
+                Console.WriteLine("2 - Recommencer la saisie du nouvel élève depuis le début");
+                Console.WriteLine("3 - Annuler la saisie et revenir au menu principal");
+                Console.WriteLine();
+                Console.Write("Votre choix : ");
+                choixAction = Console.ReadLine();
+            } while (choixAction != "1" && choixAction != "2" && choixAction != "3");
+
+            Console.WriteLine();
+            if (choixAction == "1")
+            {
+                ListeEleves.AjouterEleveDansListe(nouvelEleve);
+            }
+            else if (choixAction == "2")
+            {
+                Console.Clear();
+                CreerNouvelEleve();
+            }
+            else
+            {
+                // appeler méthode de menu pour revenir au menu principal
+                Console.WriteLine("Retour menu principal");
+            }
         }
 
         public static void SupprimerEleveDansListe()
