9e35d71aafc8e8a7ff8680ec5428def1d802e838
witzkvn <witz.kevin05@gmail.com>
2021-10-14 20:32:49 +0200

    cleaning commentaires

--- NotEdu-JKD/Eleve.cs
+++ NotEdu-JKD/Eleve.cs
@@ -30,47 +30,6 @@ namespace NotEdu_JKD
             Console.WriteLine("Date de naissance : " + Utilitaire.FormatterDateCourteString(this.DateNaissance));
         }
 
-        //public void AjouterNote(Campus campus)
-        //{
-        //    campus.ListeCours.AfficherTousLesCours(campus);
-        //    try
-        //    {
-        //        Console.Write("Quel est l'ID du cours de la note ? ");
-        //        int idCours = int.Parse(Console.ReadLine());
-        //        string  coursTitre = campus.ListeCours.ListeDesCours[idCours];
-        //        Console.Write("Quelle est la valeur de la note ? ");
-        //        double valeurNote = double.Parse(Console.ReadLine());
-        //        //!valeurNote.IsDouble ? AjouterNote();
-        //        valeurNote = valeurNote < 0 ? 0 : valeurNote;
-        //        valeurNote = valeurNote > 20 ? 20 : valeurNote;
-        //        Console.WriteLine("Quelle est l'appréciation ? (Touche Entrée si vide) ");
-        //        string appreciationNote = Console.ReadLine();
-        //        ListeNotes.Add(new Note(idCours, coursTitre, valeurNote, appreciationNote));
-
-        //        Console.WriteLine("Récapitulatif de la saisie : ");
-        //        Console.WriteLine($"Nom de l'élève : {Nom} {Prenom}");
-        //        Console.WriteLine("Cours de la note : " + coursTitre);
-        //        Console.WriteLine("Valeur de la note : " + valeurNote);
-        //        Console.WriteLine("Appréciation : " + appreciationNote);
-
-        //    }
-        //    catch
-        //    {
-        //        Console.WriteLine("Erreur lors de la saisie de la note.\nVeuillez réessayer.");
-        //        AjouterNote(campus);
-        //    }
-        //    Console.Write("La saisie est-elle correcte ? (Oui/Non) ");
-        //    string reponse = Console.ReadLine().ToLower();
-        //    if (reponse == "oui")
-        //    {
-        //        return;
-        //    }
-        //    else
-        //    {
-        //        AjouterNote(campus);
-        //    }
-        //}
-
         public void AfficherListeNotesEleve()
         {
             Console.WriteLine("Résultats scolaires :");
